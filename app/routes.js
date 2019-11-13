a3fde67ee1fc46546c1a1bedbec1fb7a67467c9d
Stephen Hill <stephen.hill9@nhs.net>
2019-11-13 13:58:07 +0000

    Added 'Current Application List', 'New Applications', 'Card Replacements', 'Change of Circumstances' and 'Individual Applicant View'...

--- app/routes.js
+++ app/routes.js
@@ -5,3 +5,25 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+// ********************************
+// MANAGE HEALTHY START VOUCHERS (VERSION 1)
+// ********************************
+
+// Capture NHSMail address (Login)
+router.post('/v1/nhs-login', function (req, res) {
+
+    var nhsLogin = req.session.data['nhs-mail-address']
+  
+    if (nhsLogin === ""){
+      req.session.data['nhs-mail-address'] = "joe.bloggs@nhs.net";
+      res.redirect('/v1/index')
+    }
+    else if (nhsLogin) {
+      res.redirect('/v1/index')
+    }
+    else {
+      res.redirect('/v1/index')
+    }
+    
+  })
