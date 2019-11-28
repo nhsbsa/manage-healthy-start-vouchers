f3b0fdfa20079fb8c05387a87e5a552245964f85
Stephen Hill <stephen.hill9@nhs.net>
2019-11-28 11:55:41 +0000

    Added breadcrumb navigation and first pass of adding an applicant

--- app/routes.js
+++ app/routes.js
@@ -29,6 +29,42 @@ router.post('/v1/nhs-login', function (req, res) {
 })
 
 // ********************************
-// MANAGE HEALTHY START VOUCHERS (VERSION 1)
+// MANAGE HEALTHY START VOUCHERS (VERSION 2)
 // ********************************
 
+// Capture NHSMail address (Login)
+router.post('/v2/nhs-login', function (req, res) {
+
+  var nhsLogin = req.session.data['nhs-mail-address']
+
+  if (nhsLogin === ""){
+    req.session.data['nhs-mail-address'] = "joe.bloggs@nhs.net";
+    res.redirect('/v2/home')
+  }
+  else if (nhsLogin) {
+    res.redirect('/v2/home')
+  }
+  else {
+    res.redirect('/v2/home')
+  }
+  
+})
+
+// Find address
+
+router.post('/v2/find-address', function (req, res) {
+
+  var name = req.session.data['1']
+  var dob = req.session.data['2']
+  var NI = req.session.data['3']
+  var houseNumber = req.session.data['4']
+  var postcode = req.session.data['5']
+
+  if (postcode) {
+    res.redirect('/v2/add-applicant-step1-1')
+  }
+  else {
+    res.redirect('/v2/add-applicant-step1')
+  }
+
+})
