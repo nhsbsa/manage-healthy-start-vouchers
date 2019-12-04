51720209f29413813846eebdbe6dc8abb49f909e
Stephen Hill <stephen.hill9@nhs.net>
2019-12-04 09:05:18 +0000

    Added moment.js and v1 of WIP on add applicant route...

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,15 @@
 // External dependencies
 const express = require('express');
 const router = express.Router();
+const moment = require('moment')
 
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+// CONSTANTS
+
+const today = new Date(Date.now());
 
 // ********************************
 // MANAGE HEALTHY START VOUCHERS (VERSION 1)
@@ -50,21 +55,93 @@ router.post('/v2/nhs-login', function (req, res) {
   
 })
 
-// Find address
+// // Find address
 
-router.post('/v2/find-address', function (req, res) {
+// router.post('/v2/find-address', function (req, res) {
 
-  var name = req.session.data['1']
-  var dob = req.session.data['2']
-  var NI = req.session.data['3']
-  var houseNumber = req.session.data['4']
-  var postcode = req.session.data['5']
+//   var name = req.session.data['1']
+//   var dob = req.session.data['2']
+//   var NI = req.session.data['3']
+//   var houseNumber = req.session.data['4']
+//   var postcode = req.session.data['5']
 
-  if (postcode) {
-    res.redirect('/v2/add-applicant-step1-1')
+//   if (postcode) {
+//     res.redirect('/v2/add-applicant-step1-1')
+//   }
+//   else {
+//     res.redirect('/v2/add-applicant-step1')
+//   }
+
+// })
+
+// Scotland
+
+router.post('/v2/do-you-live-in-scotland', function (req, res) {
+
+  var scotland = req.session.data['scotland']
+
+  if (scotland === "yes") {
+    res.redirect('/v2/applying-from-scotland')
+  }
+  else if (scotland === "no") {
+    res.redirect('/v2/add-applicant-step2')
   }
   else {
     res.redirect('/v2/add-applicant-step1')
   }
 
 })
+
+// Scotland
+
+router.post('/v2/pregnant-or-children', function (req, res) {
+
+  var pregnantorchildren = req.session.data['pregnant-or-children']
+
+  if (pregnantorchildren) {
+
+    if (pregnantorchildren === 'pregnant') {
+      res.redirect('/v2/add-applicant-step3')
+    }
+    else if (pregnantorchildren === 'children') {
+      res.redirect('/v2/add-applicant-step4')
+    }
+    else if (pregnantorchildren === 'NONE') {
+      res.redirect('/v2/add-applicant-step2-error')
+    }
+    else if (pregnantorchildren.toString() == 'pregnant,children') {
+      res.redirect('/v2/add-applicant-step3')
+    }
+    
+  } else {
+    res.redirect('/v2/add-applicant-step2')
+  }
+
+})
+
+        // Are you pregnant? > Due Date
+
+        router.post('/v2/due-date', function (req, res) {
+
+          var duedateday = req.session.data['duedateday']
+          var duedatemonth = req.session.data['duedatemonth']
+          var duedateyear = req.session.data['duedateyear']
+
+          var duedate = moment(duedateyear + '-' + duedatemonth + '-' + duedateday);
+          var tenweeksfromtoday = moment().add(10, 'weeks');
+          
+          if (duedateday && duedatemonth && duedateyear) {
+
+            if (duedate < tenweeksfromtoday) {
+              res.redirect('/v2/add-applicant-step3-error')
+            } else {
+              res.redirect('/v2/CORRECT')
+            }
+
+          }
+          else {
+            res.redirect('/v2/add-applicant-step3')
+          }
+  
+      
+      })
