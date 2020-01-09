eb66a671fff0f7ba88258fa75de8bf5e09156d37
Stephen Hill <stephen.hill9@nhs.net>
2020-01-09 12:04:33 +0000

    Added 16 - 17 apply route (Sprint 2)

--- app/routes.js
+++ app/routes.js
@@ -11,9 +11,9 @@ module.exports = router;
 
 const today = new Date(Date.now());
 
-// ********************************
+// ****************************************
 // Manage Healthy Start Scheme (VERSION 1)
-// ********************************
+// ****************************************
 
 // Capture NHSMail address (Login)
 router.post('/v1/nhs-login', function (req, res) {
@@ -33,9 +33,9 @@ router.post('/v1/nhs-login', function (req, res) {
     
 })
 
-// ********************************
+// ****************************************
 // Manage Healthy Start Scheme (VERSION 2)
-// ********************************
+// ****************************************
 
 // Capture NHSMail address (Login)
 router.post('/v2/nhs-login', function (req, res) {
@@ -146,9 +146,9 @@ router.post('/v2/pregnant-or-children', function (req, res) {
       
       })
 
-// ********************************
+// ****************************************
 // Manage Healthy Start Scheme (VERSION 3)
-// ********************************
+// ****************************************
 
 // Capture NHSMail address (Login)
 router.post('/v3/nhs-login', function (req, res) {
@@ -474,5 +474,373 @@ router.post('/v3/terms-and-conditions', function (req, res) {
   else {
     res.redirect('/v3/terms-and-conditions')
   }
+
+})
+
+// ****************************************
+// Manage Healthy Start Scheme (VERSION 4)
+// ****************************************
+
+// Capture NHSMail address (Login)
+router.post('/v4/nhs-login', function (req, res) {
+
+  var nhsLogin = req.session.data['nhs-mail-address']
+
+  if (nhsLogin === ""){
+    req.session.data['nhs-mail-address'] = "joe.bloggs@nhs.net";
+    res.redirect('/v4/home')
+  }
+  else if (nhsLogin) {
+    res.redirect('/v4/home')
+  }
+  else {
+    res.redirect('/v4/home')
+  }
+  
+})
+
+// // Find address
+
+// router.post('/v4/find-address', function (req, res) {
+
+//   var name = req.session.data['1']
+//   var dob = req.session.data['2']
+//   var NI = req.session.data['3']
+//   var houseNumber = req.session.data['4']
+//   var postcode = req.session.data['5']
+
+//   if (postcode) {
+//     res.redirect('/v4/add-applicant-step1-1')
+//   }
+//   else {
+//     res.redirect('/v4/add-applicant-step1')
+//   }
+
+// })
+
+// Scotland
+
+router.post('/v4/do-you-live-in-scotland', function (req, res) {
+
+  var scotland = req.session.data['scotland']
+
+  if (scotland === "yes") {
+    res.redirect('/v4/applying-from-scotland')
+  }
+  else if (scotland === "no") {
+    res.redirect('/v4/add-applicant-step2')
+  }
+  else {
+    res.redirect('/v4/add-applicant-step1')
+  }
+
+})
+
+// Scotland
+
+router.post('/v4/pregnant-or-children', function (req, res) {
+
+  var pregnantorchildren = req.session.data['pregnant-or-children']
+
+  if (pregnantorchildren) {
+
+    if (pregnantorchildren === 'pregnant') {
+      res.redirect('/v4/add-applicant-step3')
+    }
+    else if (pregnantorchildren === 'children') {
+      res.redirect('/v4/add-applicant-step4')
+    }
+    else if (pregnantorchildren === 'NONE') {
+      res.redirect('/v4/add-applicant-step2-error')
+    }
+    else if (pregnantorchildren.toString() == 'pregnant,children') {
+      res.redirect('/v4/add-applicant-step3')
+    }
+    
+  } else {
+    res.redirect('/v4/add-applicant-step2')
+  }
+
+})
+
+///////////
+
+// Date of birth
+
+router.post('/v4/date-of-birth', function (req, res) {
+
+  var dateofbirthday = req.session.data['dateofbirthday']
+  var dateofbirthmonth = req.session.data['dateofbirthmonth']
+  var dateofbirthyear = req.session.data['dateofbirthyear']
+
+  var dob = new Date(dateofbirthyear, dateofbirthmonth, dateofbirthday);
+  var ageDate =  new Date(today - dob.getTime())
+  var temp = ageDate.getFullYear();
+  var yrs = Math.abs(temp - 1970);
+
+  req.session.data['age'] = yrs; 
+
+
+  if (dateofbirthday && dateofbirthmonth && dateofbirthyear) {
+
+      if (yrs >= 16 && yrs < 18) {
+        res.redirect('/v4/do-they-get-vouchers')
+      } else {
+        res.redirect('/v4/not-currently-eligible-dob')
+      }
+
+  }
+  else {
+    res.redirect('/v4/date-of-birth')
+  }
+
+})
+
+// Do they get Healthy Start vouchers at the moment?
+router.post('/v4/do-they-get-vouchers', function (req, res) {
+
+  var vouchers = req.session.data['vouchers']
+
+  if (vouchers === "yes") {
+    res.redirect('/v4/already-get-vouchers')
+  }
+  else if (vouchers === "no") {
+    res.redirect('/v4/do-they-live-in-scotland')
+  }
+  else {
+    res.redirect('/v4/do-they-get-vouchers')
+  }
+
+})
+
+// Do they live in Scotland?
+
+router.post('/v4/do-they-live-in-scotland', function (req, res) {
+
+  var scotland = req.session.data['scotland']
+
+  if (scotland === "yes") {
+    res.redirect('/v4/applying-from-scotland')
+  }
+  else if (scotland === "no") {
+    res.redirect('/v4/are-they-pregnant')
+  }
+  else {
+    res.redirect('/v4/do-they-live-in-scotland')
+  }
+
+})
+
+// Are they pregnant?
+
+router.post('/v4/are-they-pregnant', function (req, res) {
+
+  var pregnant = req.session.data['pregnant']
+
+  if (pregnant === "yes") {
+    res.redirect('/v4/due-date')
+  }
+  else if (pregnant === "no") {
+    res.redirect('/v4/not-currently-eligible')
+  }
+  else {
+    res.redirect('/v4/are-they-pregnant')
+  }
+
+})
+
+router.post('/v4/due-date', function (req, res) {
+
+  var duedateday = req.session.data['duedateday']
+  var duedatemonth = req.session.data['duedatemonth']
+  var duedateyear = req.session.data['duedateyear']
+
+  var duedate = moment(duedateyear + '-' + duedatemonth + '-' + duedateday);
+  var fulltermpregnancy = moment().add(30, 'weeks'); // 40 weeks from today is a full term pregnancy - 10 weeks
+
+  var temp = moment(duedate).subtract(30, 'weeks');
+
+  var fourtytwoweeksfromtoday = moment().add(42, 'weeks');
+
+  req.session.data['preggers'] = moment(temp).format('Do MMMM YYYY');
+  
+  if (duedateday && duedatemonth && duedateyear) { // If a date has been entered...
+
+    if (duedate > fourtytwoweeksfromtoday) { // If due date is greater than 42 weeks from today...
+      res.redirect('/v4/due-date-42-weeks') // ...redirect to error screen...
+    } else {
+      res.redirect('/v4/name') // ...otherwise allow them to continue their journey!
+    }
+
+  }
+  else {
+    res.redirect('/v4/due-date')
+  }
+
+})
+
+// Do they have any children under the age of 4?
+
+router.post('/v4/children-under-four', function (req, res) {
+
+  var childrenunderfour = req.session.data['childrenunderfour']
+  var pregnant = req.session.data['pregnant']
+
+  if (pregnant === "yes" && childrenunderfour === "no") {
+    res.redirect('/v4/name')
+  } else if (pregnant === "no" && childrenunderfour === "yes") {
+    res.redirect('/v4/not-currently-eligible')
+  } else if (pregnant === "yes" && childrenunderfour === "yes") {
+    res.redirect('/v4/name')
+  } else if (pregnant === "no" && childrenunderfour === "no") {
+    res.redirect('/v4/not-eligible')
+  } else {
+    res.redirect('/v4/children-under-four')
+  }
+
+})
+
+// What is their name?
+
+router.post('/v4/name', function (req, res) {
+
+  var firstname = req.session.data['firstname']
+  var lastname = req.session.data['lastname']
+
+  if (firstname && lastname) {
+    res.redirect('/v4/address')
+  }
+  else {
+    res.redirect('/v4/name')
+  }
+
+})
+
+// What is their address?
+
+router.post('/v4/address', function (req, res) {
+
+  var addressline1 = req.session.data['addressline1']
+  var addressline2 = req.session.data['addressline2']
+  var towncity = req.session.data['towncity']
+  var postcode = req.session.data['postcode']
+
+  if (addressline1 && towncity && postcode) {
+    res.redirect('/v4/national-insurance-number')
+  }
+  else {
+    res.redirect('/v4/address')
+  }
+
+})
+
+// What is their national insurance number?
+
+router.post('/v4/national-insurance-number', function (req, res) {
+
+  var nationalinsurancenumber = req.session.data['nationalinsurancenumber']
+
+  if (nationalinsurancenumber) {
+    res.redirect('/v4/telephone-number')
+  }
+  else {
+    res.redirect('/v4/national-insurance-number')
+  }
+
+})
+
+// What is their telephone number?
+
+router.post('/v4/telephone-number', function (req, res) {
+
+  var telephonenumber = req.session.data['telephonenumber']
+
+  if (telephonenumber) {
+    res.redirect('/v4/email-address')
+  }
+  else {
+    res.redirect('/v4/telephone-number')
+  }
+
+})
+
+// What is their email address?
+
+router.post('/v4/email-address', function (req, res) {
+
+  var emailaddress = req.session.data['emailaddress']
+
+  if (emailaddress) {
+    res.redirect('/v4/terms-and-conditions')
+  }
+  else {
+    res.redirect('/v4/email-address')
+  }
+
+})
+
+// Terms and Conditions
+
+router.post('/v4/terms-and-conditions', function (req, res) {
+
+  var terms = req.session.data['terms']
+
+  var duedateday = req.session.data['duedateday']
+  var duedatemonth = req.session.data['duedatemonth']
+  var duedateyear = req.session.data['duedateyear']
+
+  var duedate = moment(duedateyear + '-' + duedatemonth + '-' + duedateday);
+  var fulltermpregnancy = moment().add(30, 'weeks'); // 40 weeks from today is a full term pregnancy - 10 weeks
+
+  if (terms) {
+
+    if (duedate > fulltermpregnancy) {
+      res.redirect('/v4/confirmation-under-10-weeks')
+    } else {
+      res.redirect('/v4/confirmation-over-10-weeks')
+    }
+
+    res.redirect('/v4/confirmation')
+  }
+  else {
+    res.redirect('/v4/terms-and-conditions')
+  }
+
+})
+
+// What is their email address?
+
+router.post('/v4/eligibility', function (req, res) {
+
+  var vouchers = req.session.data['vouchers']
+  var scotland = req.session.data['scotland']
+  var pregnant = req.session.data['pregnant']
+
+  var dateofbirthday = req.session.data['dateofbirthday']
+  var dateofbirthmonth = req.session.data['dateofbirthmonth']
+  var dateofbirthyear = req.session.data['dateofbirthyear']
+
+  var dob = new Date(dateofbirthyear, dateofbirthmonth, dateofbirthday);
+  var ageDate =  new Date(today - dob.getTime())
+  var temp = ageDate.getFullYear();
+  var yrs = Math.abs(temp - 1970);
+
+  req.session.data['age'] = yrs; 
+
+  if (vouchers === "no" && scotland === "no" && pregnant === "yes" && yrs >= 16 && yrs < 18) {
+    res.redirect('/v4/details')
+  }
+  else {
+    res.redirect('/v4/eligibility-error')
+  }
+
+})
+
+// What is their email address?
+
+router.post('/v4/details', function (req, res) {
+
+  res.redirect('/v4/confirmation-over-10-weeks')
+
 
 })
